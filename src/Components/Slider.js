b0986cb3abe9f57f19864d0f07948d43e451c1a6
hamid1882 <hussainhafeez1100@gmail.com>
2022-02-21 16:30:29 +0530

    carousel fixed

--- src/Components/Slider.js
+++ src/Components/Slider.js
@@ -5,7 +5,10 @@ import sliderImage2 from "../Images/sliderImage2.png";
 
 const Slider = () => {
   return (
-    <Carousel className="carouselStyles mb-5 container-fluid margin-auto">
+    <Carousel
+      className="carouselStyles mb-5 container-fluid margin-auto h-100 "
+      navButtonsAlwaysVisible="true"
+    >
       <img src={sliderImage} className="w-100 h-75" alt="slider" />
       <img src={sliderImage2} className="w-100 h-75" alt="slider" />
       <img src={sliderImage} className="w-100 h-75" alt="slider" />
